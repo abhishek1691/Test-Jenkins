5d8845c72fce06beaf16741caf352251aa36406f
Abhishek Shrivastav <abhishek.s@fretron.com>
2018-04-23 17:37:09 +0530

    remove unnessary print

--- src/main/java/TestManager.java
+++ src/main/java/TestManager.java
@@ -5,8 +5,6 @@ public class TestManager {
 
     public static void main(String ... args)
     {
-        System.out.println("HI jenkin!!");
-        System.out.println("This is abhishek test");
-        System.out.println("This commit send message to abhishek.s@fretron.com");
+        System.out.println("Final commit");
     }
 }
