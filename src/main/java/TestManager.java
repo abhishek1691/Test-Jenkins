2824bc79a2e49dc3b3da59334472fc285a537263
Abhishek Shrivastav <abhishek.s@fretron.com>
2018-04-23 15:22:24 +0530

    first commit

---
+++ src/main/java/TestManager.java
@@ -0,0 +1,10 @@
+/**
+ * Created by anurag on 23-Apr-18.
+ */
+public class TestManager {
+
+    public static void main(String ... args)
+    {
+        System.out.println("HI jenkin!!");
+    }
+}
