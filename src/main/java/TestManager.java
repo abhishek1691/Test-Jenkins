aef9a32134ab4e6acbbb58042f2cd20ec936001a
Abhishek Shrivastav <abhishek.s@fretron.com>
2018-04-24 12:34:13 +0530

    change print statement

--- src/main/java/TestManager.java
+++ src/main/java/TestManager.java
@@ -5,7 +5,7 @@ public class TestManager {
 
     public static void main(String ... args)
     {
-        System.out.println("I am ok!");
+        System.out.println("Hello jenkins!! with gradle");
     }
 
     public static String reverse(String word)
