4fe3532618e355cc97b0264aa92caee6e0e09e3f
abhishek1691 <abhishek.s@fretron.com>
2018-05-04 17:45:03 +0530

    test anaylze report

---
+++ src/test/java/com/fretron/Test2.java
@@ -0,0 +1,13 @@
+package com.fretron;
+
+import org.junit.Test;
+
+public class Test2 {
+
+    @Test
+    public void start()
+    {
+        System.out.print("Hii i am second test");
+        assert false;
+    }
+}
